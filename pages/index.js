30236b1b260588b6a21f9ead681ebe089a958f65
lisa710junyi <83738368+lisa710junyi@users.noreply.github.com>
2023-04-04 00:59:19 -0400

    change

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {
   AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillYoutube,
+  AiFillGithub
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
@@ -11,12 +12,7 @@ import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
 import Image from "next/image";
-import web1 from "../public/web1.png";
-import web2 from "../public/web2.png";
-import web3 from "../public/web3.png";
-import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
-import web6 from "../public/web6.png";
+import Link from "next/link";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -29,10 +25,20 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-        <section className="min-h-screen">
+        <section>
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">Hey ! Nice to meet you </h1>
             <ul className="flex items-center">
+              <li>
+                 <a className="cursor-point text-4xl px-4 mr-11" href ="https://www.youtube.com/@lisali6205/videos" target="_blank" rel="noopener noreferrer"> 
+                <AiFillYoutube /> 
+              </a>
+              </li>
+               <li>
+                 <a className="cursor-point text-4xl px-4 mr-11" href ="https://github.com/lisa-gogo" target="_blank" rel="noopener noreferrer"> 
+                <AiFillGithub /> 
+              </a>
+              </li>
               <li>
                 <BsFillMoonStarsFill
                   onClick={() => setDarkMode(!darkMode)}
@@ -42,7 +48,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
+                  href="https://docs.google.com/document/d/1H5xasVKDgAaJQdJywySmGMizgCMDUi8d/edit?usp=sharing&ouid=112025776230213363999&rtpof=true&sd=true"
                 >
                   Resume
                 </a>
@@ -50,161 +56,70 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+            <h2 className=" text-base  text-teal-600 font-medium dark:text-teal-400 md:text-3xl">
+              Junyi Li
             </h2>
-            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+            <h3 className="text-xl py-2 dark:text-white md:text-2xl">
+              Developer
             </h3>
-            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
-            </p>
-            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-              <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
-            </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
-            </div>
+            <li className="w-1/4">
+               <Link href="/projects/react"><p className=" h-12 text-2xl py-1 bg-black text-slate-100 dark:text-white" >My Project</p></Link>
+             </li>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
+            <h3 className="text-3xl py-1 dark:text-white ">My experience</h3>
           </div>
+         
+          <div className="text-xl py-1 text-blue-600">Future Fintech Labs (ftft)--AWS Cloud Certified Practitioner </div>
+          <div className="text-lg text-fuchsia-700">Software Developer Intern (Full-time On-Site) Feb 2022 - Jan 2023</div>
+          <div className="dark:text-white">* Experience in building web and mobile applications with HTML, CSS, JavaScript, ReactJS, React Native, Redux and Node.js. </div>
+          <div className="dark:text-white">* Experience in creating modern, interactive, client-facing UI with HTML/HTML5, CSS/CSS3 and JavaScript/ES6/JSON/DOM/Bootstrap.</div>
+          <div className="dark:text-white">* Experience in REST APIs, Promise and Async/Await to connect, fetch data from and send data to the server.</div>
+          <div className="dark:text-white">* Experience with backend development with Rest API, AWS lambda, API gateway, DynamoDB</div>
+          <div className="dark:text-white">* Experience in debugging and troubleshooting with the help of different debugging tools, such as React Detox and familiar with front-end unit test tools such as Mocha and Chai</div>
+          <div className="dark:text-white">* Experience with Firebase mobile tools </div>
+          <div className="text-xl py-1 text-blue-600">United Software Corporation</div>
+          <div className="text-lg text-fuchsia-700"> Trainee (Full-time) August 2021 - Nov 2021</div>
+          <div className="dark:text-white">* Experience in HTML, CSS, JavaScript and React </div>
+          <div className="dark:text-white">* Done many javascript and React projects, such as movie list, autocomplete, complex todoList</div>
+          <div className="dark:text-white">* Proficient in DOM manipulation</div>
+          <div className="dark:text-white">* Get used to React Hooks, especially useState and useEffect</div>
+          <div className="dark:text-white">* Experience in making React components, such as class component and function component</div>
+          <div className="dark:text-white">* Learning some thing about React Redux and Redux store</div>
+
           <div className="lg:flex gap-10">
             <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
               <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+              <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600 ">
+                Backend Services 
               </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <p className="text-gray-800 py-1">AWS lambda REST API</p>
+              <p className="text-gray-800 py-1">Nodejs</p>
+              <p className="text-gray-800 py-1">A little Spring Boot</p>
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
+              <h3 className="text-lg font-medium pt-8 pb-2 text-teal-600 ">
+                Frontend
               </h3>
-              <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+             
+              <h4 className="py-1">React web</h4>
+              <p className="text-gray-800 py-1">React mobile</p>
+              <p className="text-gray-800 py-1">Pure Javascript</p>
+  
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h3 className="text-lg font-medium pt-8 pb-2 ">Database</h3>
+              <h4 className="py-4">PostgreSQL</h4>
+              <p className="text-gray-800 py-1">Dynamodb</p>
+              
             </div>
           </div>
         </section>
-        <section className="py-10">
-          <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web1}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web2}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
-            </div>
-          </div>
-        </section>
+       
       </main>
     </div>
   );
---
+++ pages/projects/react.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Image from "next/image";
+import web1 from "../../public/web1.png";
+import web2 from "../../public/web2.png";
+import Link from 'next/link';
+
+export default function react() {
+  return (
+<main className=" bg-white py-10 px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+     <section className="py-10">
+           <li className="w-1/4">
+               <Link href="/"><p className="text-xl py-1 text-black dark:text-white cursor-pointer underline" >Home page</p></Link>
+             </li>
+           <h3 className="text-3xl py-10 text-center dark:text-white "><div>Project</div></h3>
+          <div>
+            
+          <div className="text-lg text-sky-600">First project, a platform that enables solo travelers to post their upcoming travel destinations, allowing other interested travelers to join them on their trip for a short period of time.</div>
+            <div className="text-lg"> I use firebase for authentication, React as frontend, React redux to manage state, and PostgreSQL in database, and Nodejs as backend.</div>
+             <div className="text-lg">You can register or login, and make your posts and check all your posts.You can check the each post detail. I am going to add chat function to this app, so two
+              users can contact with each other.
+             </div>
+             <a href ="https://neon-concha-e30ae6.netlify.app/" target="_blank" rel="noopener noreferrer" className="w-1/4">
+               <div className="text-lg cursor-point w-1/4 bg-black text-slate-100 text-center cursor-pointer py-3  m-4">Link here</div>
+              </a>
+          </div>
+            <div className="basis-1/3">
+              <Image
+                className="rounded-lg"
+                width={"200px"}
+                height={"120px"}
+                layout="responsive"
+                src={web1}
+              />
+            </div>
+          <div className="text-lg text-sky-600">Second project, a platform where shows the restaurants in the US and you can search the reastaurant you want.</div>
+            <div className="text-lg"> I use React as frontend, MongoDB in database, and Nodejs as backend.</div>
+             <div className="text-lg"> You can login and write review for the restaurant. </div>
+              <a href ="https://mellifluous-arithmetic-067e17.netlify.app/" target="_blank" rel="noopener noreferrer">
+                 <div className="text-lg cursor-point bg-black text-white w-1/4 cursor-pointer py-3 m-4 text-center">Link here</div>
+              </a>
+            <div>
+              <Image
+                className="rounded-lg object-cover"
+                width={"150px"}
+                height={"100px"}
+                layout="responsive"
+                src={web2}
+              />
+            </div>
+      
+        </section>
+</main>
+  )
+}
